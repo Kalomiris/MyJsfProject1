b1f92ce062e1a535e291593d090eb4cdd57f4fd1
kalomoio <i.kalomoiris@afse.eu>
2018-08-01 22:16:38 +0300

    remove randomDateOfBirth and randBetween from CustomerService to DataBase

--- src/main/java/CustomerService.java
+++ src/main/java/CustomerService.java
@@ -5,9 +5,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 
@@ -16,7 +14,7 @@ import java.util.List;
 public class CustomerService implements Serializable{
 
     private static final long serialVersionUID = -6851187912764155763L;
-    private List<Customer> list = new ArrayList<Customer>();
+    private List<Customer> list = new ArrayList<>();
 
     @PostConstruct
     public void createCustomer() {
@@ -25,16 +23,5 @@ public class CustomerService implements Serializable{
 
     public List<Customer> getList() {
         return list;
-    }
-
-    private Date randomDateOfBirth() {
-        int day = randBetween(1,28);
-        int month = randBetween(1,12);
-        int year = randBetween(1900, 2018);
-        return java.sql.Date.valueOf(LocalDate.of(year, month, day));
-    }
-
-    private int randBetween(int start, int end) {
-        return start + (int) Math.round(Math.random() * (end - start));
     }
 }
